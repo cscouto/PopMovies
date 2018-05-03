d13d67bb986adcb17faf232c14863c638083c385
Tiago Do Couto <cs.couto@outlook.com>
2018-05-03 12:22:48 -0500

    [MainActivity] - web requests

--- app/src/main/java/com/coutocode/popmovies/MainActivity.java
+++ app/src/main/java/com/coutocode/popmovies/MainActivity.java
@@ -4,12 +4,28 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    private MoviesService moviesService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+
+        moviesService = new MoviesService();
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        callPopularMovies();
     }
 
     @Override
@@ -31,12 +56,61 @@ public class MainActivity extends AppCompatActivity {
 
         switch (id){
             case R.id.action_most_popular:
+                callPopularMovies();
                 return true;
             case  R.id.action_highest_rated:
+                callHighestRatedMovies();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void updateUI(ArrayList<Movie> movies){
+        PosterAdapter adapter = new PosterAdapter(movies);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    private void callPopularMovies(){
+        Call call = moviesService.getMostPopular();
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                if (response.isSuccessful()){
+                    updateUI((ArrayList<Movie>) response.body().results);
+                }else{
+                    Toast.makeText(MainActivity.this,
+                            response.message(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this,
+                        R.string.request_failed, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void callHighestRatedMovies(){
+        Call call = moviesService.getMostHighestRatedMovies();
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                if (response.isSuccessful()){
+                    updateUI((ArrayList<Movie>) response.body().results);
+                }else{
+                    Toast.makeText(MainActivity.this,
+                            response.message(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this,
+                        R.string.request_failed, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
