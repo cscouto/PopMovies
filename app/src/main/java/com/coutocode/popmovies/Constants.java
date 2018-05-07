b567ca45ef49f255e1660c65826cc2884e692c9e
Tiago Do Couto <cs.couto@outlook.com>
2018-05-07 13:38:53 -0500

    [DetailActivity] - implemented calls to populate reviews and trailers

--- app/src/main/java/com/coutocode/popmovies/Constants.java
+++ app/src/main/java/com/coutocode/popmovies/Constants.java
@@ -2,7 +2,7 @@ package com.coutocode.popmovies;
 
 public class Constants {
     static String BASE_URL = "https://api.themoviedb.org/3/";
-    static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
-    static String IMAGE_SIZE_500 = "w500/";
+    public static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+    public static String IMAGE_SIZE_500 = "w500/";
     static String DETAIL_EXTRA_KEY = "movie";
 }
--- app/src/main/java/com/coutocode/popmovies/DetailActivity.java
+++ app/src/main/java/com/coutocode/popmovies/DetailActivity.java
@@ -1,17 +1,31 @@
 package com.coutocode.popmovies;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.coutocode.popmovies.apdater.ReviewAdapter;
+import com.coutocode.popmovies.apdater.TrailerAdapter;
+import com.coutocode.popmovies.model.Movie;
+import com.coutocode.popmovies.model.Review;
+import com.coutocode.popmovies.model.ReviewResponse;
+import com.coutocode.popmovies.model.Trailer;
+import com.coutocode.popmovies.model.TrailerResponse;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.coutocode.popmovies.Constants.IMAGE_BASE_URL;
 import static com.coutocode.popmovies.Constants.IMAGE_SIZE_500;
@@ -28,6 +42,12 @@ public class DetailActivity extends AppCompatActivity {
     TextView textViewRelease;
     @BindView(R.id.textViewSynopsis)
     TextView textViewSynopsis;
+    @BindView(R.id.recyclerViewTrailer)
+    RecyclerView recyclerViewTrailer;
+    @BindView(R.id.recyclerViewReview)
+    RecyclerView recyclerViewReview;
+
+    private MoviesService moviesService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +60,16 @@ public class DetailActivity extends AppCompatActivity {
 
         Movie movie = getIntent().getExtras().getParcelable(Constants.DETAIL_EXTRA_KEY);
 
+        LinearLayoutManager layoutManagerReview = new LinearLayoutManager(this);
+        recyclerViewReview.setLayoutManager(layoutManagerReview);
+        LinearLayoutManager layoutManagerTrailer = new LinearLayoutManager(this);
+        recyclerViewTrailer.setLayoutManager(layoutManagerTrailer);
+
+        moviesService = new MoviesService();
+
+        callReviews(String.valueOf(movie.id));
+        callTrailers(String.valueOf(movie.id));
+
         String path = IMAGE_BASE_URL + IMAGE_SIZE_500 + movie.poster_path;
         Picasso.with(this)
                 .load(path)
@@ -47,7 +77,7 @@ public class DetailActivity extends AppCompatActivity {
                 .error(R.drawable.noimage)
                 .into(imageViewPoster);
         textViewTitle.setText(movie.title);
-        textViewRate.setText(String.valueOf(movie.vote_average));
+        textViewRate.setText(String.valueOf(movie.vote_average) + "/10");
         textViewRelease.setText(movie.release_date);
         textViewSynopsis.setText(movie.overview);
     }
@@ -61,5 +91,57 @@ public class DetailActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void callReviews(String movieId){
+       Call<ReviewResponse> call =  moviesService.listReviews(movieId);
+       call.enqueue(new Callback<ReviewResponse>() {
+           @Override
+           public void onResponse(Call<ReviewResponse> call, Response<ReviewResponse> response) {
+               if (response.isSuccessful()){
+                   updateReviews(response.body().results);
+               }else{
+                   Toast.makeText(DetailActivity.this,
+                           response.message(), Toast.LENGTH_LONG).show();
+               }
+           }
+
+           @Override
+           public void onFailure(Call<ReviewResponse> call, Throwable t) {
+               Toast.makeText(DetailActivity.this,
+                       R.string.request_failed, Toast.LENGTH_LONG).show();
+           }
+       });
+    }
+
+    private void callTrailers(String movieId){
+        Call<TrailerResponse> call = moviesService.listTrailers(movieId);
+        call.enqueue(new Callback<TrailerResponse>() {
+            @Override
+            public void onResponse(Call<TrailerResponse> call, Response<TrailerResponse> response) {
+                if (response.isSuccessful()){
+                    updateTrailers(response.body().results);
+                }else{
+                    Toast.makeText(DetailActivity.this,
+                            response.message(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TrailerResponse> call, Throwable t) {
+                Toast.makeText(DetailActivity.this,
+                        R.string.request_failed, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void updateReviews(List<Review> reviews){
+        ReviewAdapter adapter = new ReviewAdapter(reviews);
+        recyclerViewReview.setAdapter(adapter);
+    }
+
+    private void updateTrailers(List<Trailer> trailers){
+        TrailerAdapter adapter = new TrailerAdapter(trailers);
+        recyclerViewTrailer.setAdapter(adapter);
     }
 }
